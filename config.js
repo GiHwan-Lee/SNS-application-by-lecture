8f9f587a9cc24c2d70f2d83bfecbcb1b1935f47d
Gi Hwan <ass302@naver.com>
2023-08-21 21:56:30 +0900

    Added comments to config.js and socket.js files

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
+// 'dotenv' 모듈을 임포트하여 .env 파일로부터 환경 변수를 읽어옵니다.
 import dotenv from "dotenv";
 dotenv.config();
 
+// 필요한 환경 변수 또는 기본값을 가져오는 함수입니다. 만약 env 값이 없다면, default로 undefined가 출력 됩니다.
+// 그리고 만약 두 값 모두 제공되지 않으면 오류를 발생시킵니다.
 function required(key, defaultValue = undefined) {
   const value = process.env[key] || defaultValue;
   if (value == null) {
@@ -8,31 +11,33 @@ function required(key, defaultValue = undefined) {
   }
   return value;
 }
-//required는 실수로 환경변수에 있지도 않은 데이터를 입력했을 때를 방지하기 위해 작성한 것이다.
-//process.env[key] 값이 없다면 default로 설정한 undefined가 출력된다.
-//혹은 null이라면 Error를 던져준다.
 
 export const config = {
   jwt: {
-    secretKey: required("JWT_SECRET"),
-    expiresInSec: parseInt(required("JWT_EXPIRES_SEC", 17200)),
+    // JWT 인증 설정입니다.
+    secretKey: required("JWT_SECRET"), // JWT 서명 및 검증을 위한 시크릿 키
+    expiresInSec: parseInt(required("JWT_EXPIRES_SEC", 17200)), // JWT 토큰의 만료 시간
   },
 
   bcrypt: {
-    saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 12)),
+    // 비밀번호 해싱을 위한 bcrypt 설정입니다.
+    saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 12)), // bcrypt 해싱을 위한 솔트 라운드 수
   },
 
   db: {
-    host: required("DB_HOST"),
-    user: required("DB_USER"),
-    database: required("DB_DATABASE"),
-    password: required("DB_PASSWORD"),
-    port: required("DB_PORT"),
+    // 데이터베이스 설정입니다.
+    host: required("DB_HOST"), // 데이터베이스 호스트 이름
+    user: required("DB_USER"), // 데이터베이스 사용자 이름
+    database: required("DB_DATABASE"), // 사용하는 데이터베이스의 이름
+    password: required("DB_PASSWORD"), // 데이터베이스 비밀번호
+    port: required("DB_PORT"), // 데이터베이스 포트 번호
   },
 
+  // 서버 포트 설정입니다.
   port: parseInt(required("PORT", 8080)),
 
   cors: {
-    allowedOrigin: required("CORS_ALLOW_ORIGIN"),
+    // CORS(교차 출처 리소스 공유) 설정입니다.
+    allowedOrigin: required("CORS_ALLOW_ORIGIN"), // API에 접근이 허용되는 출처를 지정합니다.
   },
 };
--- connection/socket.js
+++ connection/socket.js
@@ -2,14 +2,17 @@ import { Server } from "socket.io";
 import jwt from "jsonwebtoken";
 import { config } from "../config.js";
 
+// Socket 클래스 정의
 class Socket {
   constructor(server) {
+    // 소켓 서버를 초기화하며, CORS 설정을 적용합니다.
     this.io = new Server(server, {
       cors: {
         origin: config.cors.allowedOrigin,
       },
     });
 
+    // 각 소켓 연결에 대한 미들웨어로, JWT 토큰을 검증하여 인증을 수행합니다.
     this.io.use((socket, next) => {
       const token = socket.handshake.auth.token;
       if (!token) {
@@ -23,16 +26,24 @@ class Socket {
       });
     });
 
+    // 새로운 소켓 연결이 발생했을 때 콘솔에 메시지를 출력합니다.
     this.io.on("connection", () => console.log("Socket client connected"));
   }
 }
 
+// 전역 소켓 인스턴스
 let socket;
+
+// 소켓을 초기화하는 함수입니다.
+// initSocket은 서버 시작 시 한 번만 호출되어야 합니다.
 export function initSocket(server) {
   if (!socket) {
     socket = new Socket(server);
   }
 }
+
+// 초기화된 소켓 인스턴스를 반환하는 함수입니다.
+// initSocket 함수가 먼저 호출되어야 합니다.
 export function getSocketIO() {
   if (!socket) {
     throw new Error("Please call init first");
